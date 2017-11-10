240cc83c181146ad0253ec3d155eac0ad28bc08f
Nathan Huberty <nathan.huberty@orange.fr>
2017-11-10 12:22:43 +0100

    photo good

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
+  mount Attachinary::Engine => "/attachinary"
 
   resources :cocktails, only: %i(index new show create) do
     resources :doses, only: %i(new create)
   end
   resources :doses, only: %i(destroy)
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+# Ingredient.destroy_all
 
 Ingredient.create(name: "Lemon")
 Ingredient.create(name: "Ice")
